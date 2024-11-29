277a3caa85a7df69740e0a4990aff9b94a5917ec
olleikali <ali_olleik@hotmail.com>
2024-11-29 23:40:55 +0100

    change

--- terraform_remote-backend/backend.tf
+++ terraform_remote-backend/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "week6-ao-bucket-terraform"
+    bucket         = ""
     key            = "dev/terraform.tfstate"
     region         = "eu-north-1"
-    dynamodb_table = "terraform-lock-AO"
+    dynamodb_table = ""
     encrypt        = true
   }
 }
